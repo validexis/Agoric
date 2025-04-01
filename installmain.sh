35b5ff074a8864b466af80f7a6fe96d590df880d
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-04-01 22:08:57 +0300

    Update installmain.sh

--- installmain.sh
+++ installmain.sh
@@ -22,10 +22,10 @@ echo "export PATH=$PATH:/usr/local/go/bin:/usr/local/bin:$HOME/go/bin" >> $HOME/
 source $HOME/.bash_profile
 
 cd $HOME
-rm -rf agoric-upgrade-18
-git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-18a
-cd agoric-upgrade-18a
-git checkout agoric-upgrade-18a
+rm -rf agoric-upgrade-18а
+git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-19
+cd agoric-upgrade-19
+git checkout agoric-upgrade-19
 
 yarn install && yarn build
 (cd packages/cosmic-swingset && make)
