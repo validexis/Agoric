7ce937c316dabb811f70a86010f09aafd86ac582
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-04-01 22:08:20 +0300

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd $HOME
-rm -rf agoric-upgrade-18
-git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-18a
-cd agoric-upgrade-18a
-git checkout agoric-upgrade-18a
+rm -rf agoric-upgrade-18а
+git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-19
+cd agoric-upgrade-19
+git checkout agoric-upgrade-19
 
 yarn install && yarn build
 
